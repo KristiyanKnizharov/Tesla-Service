fbaa13b0bb92c56ffb6f1467a63869850b914a51
KristiyanK <mejik99@abv.bg>
2020-12-17 05:54:47 +0200

    Create InsuranceService and Insurance/InfoInsuranceModel.cs

---
+++ Services/TeslaService.Services.Data/Contracts/IInsuranceService.cs
@@ -0,0 +1,21 @@
+﻿namespace TeslaService.Services.Data.Contracts
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using TeslaService.Data.Models;
+    using TeslaService.Web.ViewModels.Insurance;
+
+    public interface IInsuranceService
+    {
+        Task CreateInsuranceAsync(InfoInsuranceModel insuranceModel);
+
+        void DeleteInsurance(string insuranceId);
+
+        bool IsItInsuranceCreated(string insuranceId);
+
+        IEnumerable<Insurance> GetAllInsurances();
+
+        int CountInsurances();
+    }
+}
---
+++ Web/TeslaService.Web.ViewModels/Insurance/InfoInsuranceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TeslaService.Web.ViewModels.Insurance
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class InfoInsuranceModel
+    {
+    }
+}
