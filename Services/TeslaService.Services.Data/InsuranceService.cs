c476f124d9546a4229965c610b48fff09ddfabc0
KristiyanK <mejik99@abv.bg>
2020-12-17 06:02:42 +0200

    Extend InsuranceService.cs and Insurance/InfoInsuranceModel.cs

---
+++ Services/TeslaService.Services.Data/InsuranceService.cs
@@ -0,0 +1,47 @@
+﻿namespace TeslaService.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TeslaService.Data.Common.Repositories;
+    using TeslaService.Data.Models;
+    using TeslaService.Services.Data.Contracts;
+    using TeslaService.Web.ViewModels.Insurance;
+
+    public class InsuranceService : IInsuranceService
+    {
+        private readonly IRepository<Insurance> insuranceRepository;
+
+        public InsuranceService(IRepository<Insurance> insuranceRepository)
+        {
+            this.insuranceRepository = insuranceRepository;
+        }
+
+        public int CountInsurances()
+        {
+            return this.insuranceRepository.AllAsNoTracking().Count();
+        }
+
+        public Task CreateInsuranceAsync(InfoInsuranceModel insuranceModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteInsurance(string insuranceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Insurance> GetAllInsurances()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsItInsuranceCreated(string insuranceId)
+        {
+            return this.insuranceRepository.AllAsNoTracking().Any(x => x.Id == insuranceId);
+        }
+    }
+}
--- Web/TeslaService.Web.ViewModels/Insurance/InfoInsuranceModel.cs
+++ Web/TeslaService.Web.ViewModels/Insurance/InfoInsuranceModel.cs
@@ -1,10 +1,19 @@
 ﻿namespace TeslaService.Web.ViewModels.Insurance
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text;
+    using System.ComponentModel.DataAnnotations;
 
     public class InfoInsuranceModel
     {
+        public string Id { get; set; }
+
+        [MaxLength(30)]
+        public string VinNumber { get; set; }
+
+        public string DateOfStart { get; set; }
+
+        public string DateOfEnd { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
     }
 }
